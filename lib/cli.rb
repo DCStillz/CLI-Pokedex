f30e64c4cd56f40c12388a63477328d725950d3d
DCStillz <dylan@dcokerstills.com>
2021-02-22 16:13:14 -0500

    adds error handling if user enters wrong parameter

--- lib/cli.rb
+++ lib/cli.rb
@@ -16,6 +16,20 @@ Please input a number between 1 and 898 to see that Pokemon"
 
     def attribute_options(pokemon)
         puts "Please choose whether you would like to see #{pokemon.name}'s height or weight by typing 'height' or 'weight'"
+        self.user_selection(pokemon)
+    end
+
+    def user_selection(pokemon)
+        input = gets.strip
+        if input == "height"
+            puts "#{pokemon.name} is #{pokemon.height * 10} cm tall."
+        elsif input == "weight"
+            puts "#{pokemon.name} weighs #{pokemon.weight * 0.1} kgs."
+        else input != "weight" && input != "height"
+            puts "That selection was invalid"
+            self.attribute_options(pokemon)
+            binding.pry
+        end
     end
 
 end
