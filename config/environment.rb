22124920a4669be74b2b6890c6b09200dda5257c
DCStillz <dylan@dcokerstills.com>
2021-02-21 19:44:36 -0500

    fix LoadError message

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,5 @@
 require 'pry'
 
-require_relative('..lib/api')
-require_relative('..lib/cli')
-require_relative('..lib/model1')
+require_relative('../lib/api')
+require_relative('../lib/cli')
+require_relative('../lib/model1')
