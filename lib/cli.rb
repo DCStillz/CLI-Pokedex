9b5a7284b82ccf7189fe342c3edc4e0e3c7da1a4
DCStillz <dylan@dcokerstills.com>
2021-02-21 19:52:05 -0500

    add ask for zip method for user input

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,13 @@
 class Cli
 
     def welcome
-        puts "Welcome to my movie locator! This app will allow you to find what movies are playing at theaters in Raleigh, North Carolina!"
+        puts "Welcome to my movie locator! This app will allow you to find which theaters are playing what movies within 10 miles of your zip code!"
+    end
+
+    def ask_for_zip
+        puts "Please enter your zip code."
+        input = gets.strip
+        #call the API
     end
 
 
