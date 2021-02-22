8ce15315350a32a4e1b00466487867830f905599
DCStillz <dylan@dcokerstills.com>
2021-02-21 19:40:41 -0500

    welcome users to the application

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,3 +1,8 @@
-class CLI
+class Cli
+
+    def welcome
+        puts "Welcome to my movie locator! This app will allow you to find what movies are playing at theaters in Raleigh, North Carolina!"
+    end
+
 
 end
