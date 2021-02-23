0dbb2bba2a8bf0aab6a4dafb510474c0ca9a7dc6
DCStillz <dylan@dcokerstills.com>
2021-02-23 01:47:26 -0500

    adds class variable to store instances

--- lib/model1.rb
+++ lib/model1.rb
@@ -2,11 +2,18 @@ class Model_1
 
     attr_accessor :name, :height, :weight
 
+    @@all = []
+
     def initialize (hash)
         hash.each do |key, value|
             
             self.send("#{key}=", value) if self.respond_to?("#{key}=")
-        end    
+        end  
+        self.all
+    end
+
+    def self.all
+        @@all
     end
         
 
